b40d930eaecaf8730d704aca4465961717f612c3
apollo <matt@mattparlmer.com>
2017-10-10 13:34:57 -0500

    adds min return test to analysis::percentile::element()

--- tests/analysis/percentile.cpp
+++ tests/analysis/percentile.cpp
@@ -4,8 +4,10 @@
 // Unit tests for percentile
 BOOST_AUTO_TEST_SUITE(AnalysisPercentile)
 
+// percentile::index()
+
 BOOST_AUTO_TEST_CASE(VerifyIndexZeroReturnValue) {
-    // Verify percentile::index() return value";
+    // Verify percentile::index() return value
     auto expected = 0;
     auto actual = omi::analysis::percentile::index(0, 1);
 
@@ -13,7 +15,7 @@ BOOST_AUTO_TEST_CASE(VerifyIndexZeroReturnValue) {
 }
 
 BOOST_AUTO_TEST_CASE(VerifyIndexLessThanOneHundredReturnValue) {
-    // Verify percentile::index() return value";
+    // Verify percentile::index() return value
     auto expected = 0;
     auto actual = omi::analysis::percentile::index(50, 1);
 
@@ -36,10 +38,20 @@ BOOST_AUTO_TEST_CASE(VerifyIndexReturnValueTen) {
     BOOST_CHECK_EQUAL(expected, actual);
 }
 
+// percentile::element()
+
 BOOST_AUTO_TEST_CASE(VerifyElementZeroReturnValue) {
-    // Verify value
-    auto expected = 0.0; // 0 based
+    // Verify percentile::element() return value
+    auto expected = 0.0;
     auto vector = std::vector<double>{0.0, 0.0, 0.0, 0.0};
+    auto actual = omi::analysis::percentile::element(vector, 0.0);
+
+    BOOST_CHECK_EQUAL(expected, actual);
+}
+
+BOOST_AUTO_TEST_CASE(VerifyElementMinReturnValue) {
+    auto expected = 0.0;
+    auto vector = std::vector<double>{0.0, 1.0, 2.0, 3.0};
     auto actual = omi::analysis::percentile::element(vector, 0.0);
 
     BOOST_CHECK_EQUAL(expected, actual);
