bcd730a79e7fe8b567f33472147ec0a4242b5335
omi-dev <contact.omi@protonmail.com>
2018-07-19 22:57:06 -0500

    Remove C++17 deprecated std::result_of_t

--- omi/collection/transform.hpp
+++ omi/collection/transform.hpp
@@ -15,7 +15,7 @@ namespace omi::collection {
 // Transform vector acoording to operation
 template <typename container, typename function, typename type = typename container::value_type>
 auto transform(const container &elements, function && functor) {
-    std::vector<std::result_of_t<function(type)>> result(elements.size());
+    std::vector<decltype(std::declval<function>()(std::declval<type>()))> result(elements.size());
     std::transform(std::begin(elements), std::end(elements), std::begin(result), functor);
     return result;
 }
